616f919fc8e6fd9f5e46bf08d4089c7d44cf8cbf
Rasel006 <rizurasel.cse@gmail.com>
2023-12-19 01:38:15 +0600

    assignment4 done

---
+++ TaskManagementSystem/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from TaskModel.models import add_task
+
+def home(request):
+    data = add_task.objects.all()
+    return render(request, 'home.html',{'data': data})
